b61ccf8992a8456d06316c7e9a85b3e3403ac847
Wojciech Mrożek <wojtekmrozek717@gmail.com>
2025-10-13 18:40:41 +0200

    Update deploy workflow and ECS task definition

--- backend_Django/web/views.py
+++ backend_Django/web/views.py
@@ -1,5 +1,8 @@
 # web/views.py
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
 
 def home(request):
     return HttpResponse("Witaj na edublinkier.com! 🚀")
+
+def health(request):
+    return JsonResponse({"status": "ok"})
