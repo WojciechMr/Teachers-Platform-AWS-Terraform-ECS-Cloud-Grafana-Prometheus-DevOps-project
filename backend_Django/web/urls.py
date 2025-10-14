1449d3b52cd4abdbf8d52585c13640f0577bf637
Wojciech Mrożek <wojtekmrozek717@gmail.com>
2025-10-14 19:01:02 +0200

    ulrs.py change

--- backend_Django/web/urls.py
+++ backend_Django/web/urls.py
@@ -1,10 +1,11 @@
 # web/urls.py
 from django.urls import path
-from .views import home, health
+from .views import home
 from django.contrib import admin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('health', health, name='health')
+    path('', home, name='home'),
 ]
--- backend_Django/web/views.py
+++ backend_Django/web/views.py
@@ -2,10 +2,11 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET
+
 def home(request):
     return HttpResponse("Witaj na edublinkier.com! 🚀")
 
 @csrf_exempt
 @require_GET
-def health(request):
+def home(request):
     return HttpResponse("OK", status=200)
