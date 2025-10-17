a99c01cef956855ccb8a304a842df8e0b2ab64e1
Wojciech Mrożek <wojtekmrozek717@gmail.com>
2025-10-17 18:16:34 +0200

    Add /healthz endpoint and update ALB health check

--- backend_Django/web/urls.py
+++ backend_Django/web/urls.py
@@ -2,9 +2,13 @@
 from django.urls import path
 from .views import home
 from django.contrib import admin
+from django.http import HttpResponse
 
+def health_check(request):
+    return HttpResponse("ok", status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('healthz', health_check),  # <--- dodany health check
 ]
