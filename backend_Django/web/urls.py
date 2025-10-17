f02d89b8ce2c688e2957e551618b21c3e21b7b8a
Wojciech Mrożek <wojtekmrozek717@gmail.com>
2025-10-17 18:29:07 +0200

    ulrs.py change

--- backend_Django/web/urls.py
+++ backend_Django/web/urls.py
@@ -1,14 +1,21 @@
 # web/urls.py
-from django.urls import path
 from django.http import HttpResponse
-from .views import home
-from django.contrib import admin
+from django.urls import path
 
+# Funkcja health check
 def health_check(request):
     return HttpResponse("ok", status=200)
+
+# Istniejące URL patterns
+from .views import home
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('healthz', health_check),
+]
+
+# Dodajemy health check
+urlpatterns += [
+    path("healthz", health_check),
 ]
