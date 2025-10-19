019c7909f3ad4a4480045716a53af7ed4655dfd1
Wojciech Mrożek <wojtekmrozek717@gmail.com>
2025-10-19 14:24:48 +0200

    Django_secret_k change

--- Infra_Terraform/ecs.tf
+++ Infra_Terraform/ecs.tf
@@ -1,4 +1,12 @@
-# ECS Cluster
 resource "aws_ecs_cluster" "edu_ecs_cluster" {
   name = "edu-ecs-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "disabled"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
