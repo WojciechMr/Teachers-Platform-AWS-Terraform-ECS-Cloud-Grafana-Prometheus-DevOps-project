80d7ede2234498411745971a24c1b37001c0f132
Wojciech Mrożek <wojtekmrozek717@gmail.com>
2025-11-15 10:37:08 +0100

    the change of local database to RDS database in dev.py

--- backend_Django/web/settings/base.py
+++ backend_Django/web/settings/base.py
@@ -89,3 +89,5 @@ EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True").lower() in ("true", "1", "yes")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- backend_Django/web/settings/dev.py
+++ backend_Django/web/settings/dev.py
@@ -4,12 +4,12 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 # 🔹 Lokalna baza SQLite do testów dev
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 # 🔹 Statyczne pliki lokalnie
 STATICFILES_DIRS = [
